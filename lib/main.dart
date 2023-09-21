619f8f0be82fbbe8bbdcc7cc00cb6ab0241a487c
banksdaniel <banks13@sempreceub.com>
2023-09-21 20:37:33 -0300

    ♻️ Adding mod and componentization logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,38 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_project/screen/auth/login_screen.dart';
 void main() {
-  runApp(const GlobalApp());
+  runApp(const GloboApp());
 }
 
-class GlobalApp extends StatelessWidget {
-  const GlobalApp({super.key});
+class GloboApp extends StatelessWidget {
+  const GloboApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         final currentFocus = FocusScope.of(context);
-        FocusManager.instance.primaryFocus?.unfocus();
+        if (!currentFocus.hasPrimaryFocus) {
+          FocusManager.instance.primaryFocus?.unfocus();
+        }
       },
       child: MaterialApp(
         theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(
-                seedColor: Colors.deepPurple, brightness: Brightness.light),
-            useMaterial3: true),
-        darkTheme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(
-                seedColor: Colors.purple, brightness: Brightness.dark),
-            useMaterial3: true),
-        home: const Home(),
-      ),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  bool _showPassword = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                'GloboApp',
-                style: TextStyle(fontSize: 40),
-              ),
-              const SizedBox(height: 16),
-              const TextField(
-                decoration: InputDecoration(filled: true, labelText: 'E-mail'),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                  decoration: InputDecoration(
-                filled: true,
-                labelText: 'Senha',
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _showPassword = !_showPassword;
-                    });
-                  },
-                  icon: Icon(
-                      _showPassword ? Icons.visibility : Icons.visibility_off),
-                ),
-                
-              )),
-              
-            ],
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+            brightness: Brightness.light,
           ),
+          useMaterial3: true,
         ),
+        darkTheme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.purple,
+            brightness: Brightness.dark,
+          ),
+          useMaterial3: true,
+        ),
+        themeMode: ThemeMode.dark,
+        home: const LoginScreen(),
       ),
     );
   }
